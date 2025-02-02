539f3a31ebee3b650d5a9a05a92f5ed733fd203a
danielpinoy <almirante.danieljohn@gmail.com>
2025-02-02 18:50:01 +0200

    small fixes
    -miss spelling fixes

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 Project Name: Historic Movies Client
 
 -The Historic Movies Client is a web application that provides users with a curated collection of historical movies. It aims to offer an immersive and educational experience by showcasing films that depict significant events, figures, and eras throughout history.
@@ -8,25 +7,23 @@ How to get the project running
 -open link and register as a user
 https://kaleidoscopic-tiramisu-9e52da.netlify.app/login
 
-
 Project dependencies (e.g., JavaScript version, ESLint rules)
 
-  -JavaScript 
-  -@reduxjs/toolkit
-  -bootstrap
-  -prop-types
-  -react
-  -react-bootstrap
-  -react-dom
-  -react-redux
-  -react-router
-  -react-router-dom
-  -react-scripts
-  -redux
-  -ESLint (Default rule)
-
+-JavaScript
+-@reduxjs/toolkit
+-bootstrap
+-prop-types
+-react
+-react-bootstrap
+-react-dom
+-react-redux
+-react-router
+-react-router-dom
+-react-scripts
+-redux
+-ESLint (Default rule)
 
 Which API the project uses
 
 -From render
-https://history-movie-api.onrender.com
+https://history-movie-api.vercel.app
--- src/asyncThunk/userAsyncThunks.js
+++ src/asyncThunk/userAsyncThunks.js
@@ -3,187 +3,195 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 const storedToken = localStorage.getItem("token");
 const storedUser = JSON.parse(localStorage.getItem("user"));
 
-const API_URL = "https://history-movie-api.vercel.app/";
+const API_URL = "https://history-movie-api.vercel.app";
 // Async thunks
 export const signupUser = createAsyncThunk(
-    "user/signup",
-    async ({ Username, Password, Email, Birthday }, { rejectWithValue }) => {
-        try {
-            const data = { Username, Password, Email, Birthday };
-            const response = await fetch(`${API_URL}/register`, {
-                method: "POST",
-                body: JSON.stringify(data),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
+  "user/signup",
+  async ({ Username, Password, Email, Birthday }, { rejectWithValue }) => {
+    try {
+      const data = { Username, Password, Email, Birthday };
+      const response = await fetch(`${API_URL}/register`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message);
-            }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
 
-            const signupUser = await response.json();
-            return signupUser;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+      const signupUser = await response.json();
+      return signupUser;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 export const loginUser = createAsyncThunk(
-    "user/login",
-    async ({ username, password }, { rejectWithValue }) => {
-        try {
-            const data = { Username: username, Password: password };
-            const response = await fetch(`${API_URL}/login`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            });
+  "user/login",
+  async ({ username, password }, { rejectWithValue }) => {
+    try {
+      const data = { Username: username, Password: password };
+      const response = await fetch(`${API_URL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message);
-            }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
 
-            const userData = await response.json();
-            return userData;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+      const userData = await response.json();
+      return userData;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 export const editUser = createAsyncThunk(
-    "user/edit",
-    async ({ userData, updatedUserData, token }, { rejectWithValue }) => {
-        try {
-            const response = await fetch(`${API_URL}/user/${userData.Username}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify(updatedUserData),
-            });
+  "user/edit",
+  async ({ userData, updatedUserData, token }, { rejectWithValue }) => {
+    try {
+      const response = await fetch(`${API_URL}/user/${userData.Username}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(updatedUserData),
+      });
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message);
-            }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
 
-            const updatedUser = await response.json();
-            return updatedUser;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+      const updatedUser = await response.json();
+      return updatedUser;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 export const deleteUser = createAsyncThunk(
-    "user/delete",
-    async ({ user, token }, { rejectWithValue }) => {
-        try {
-            const response = await fetch(`${API_URL}/user/${user._id}`, {
-                method: "DELETE",
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                },
-            });
+  "user/delete",
+  async ({ user, token }, { rejectWithValue }) => {
+    try {
+      const response = await fetch(`${API_URL}/user/${user._id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
 
-            if (!response.ok) {
-                throw new Error("Failed to delete the user.");
-            }
+      if (!response.ok) {
+        throw new Error("Failed to delete the user.");
+      }
 
-            localStorage.clear();
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+      localStorage.clear();
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 export const addFavoriteMovieToUser = createAsyncThunk(
-    "user/addFavoriteMovieToUser",
-    async ({ userId, movieId }, { rejectWithValue }) => {
-        try {
-            console.log(userId + " " + movieId);
+  "user/addFavoriteMovieToUser",
+  async ({ userId, movieId }, { rejectWithValue }) => {
+    try {
+      console.log(userId + " " + movieId);
 
-            const response = await fetch(`${API_URL}/user/addfavorite`, {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${storedToken}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ userId: userId, movieId }),
-            });
+      const response = await fetch(`${API_URL}/user/addfavorite`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${storedToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: userId, movieId }),
+      });
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message);
-            }
-            const updatedUser = await response.json();
-            return updatedUser;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
+      const updatedUser = await response.json();
+      return updatedUser;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 export const changePassword = createAsyncThunk(
-    "user/changePassword",
-    async ({ userData, passwordData }, { rejectWithValue }) => {
-        try {
-            const response = await fetch(`${API_URL}/user/${userData.Username}/change-password`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
-                },
-                body: JSON.stringify(passwordData),
-            });
-
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message);
-            }
-
-            await response.json(); // Consume the response body
-            return { success: true, message: "Password changed successfully" };
-        } catch (error) {
-            return rejectWithValue(error.message);
+  "user/changePassword",
+  async ({ userData, passwordData }, { rejectWithValue }) => {
+    try {
+      const response = await fetch(
+        `${API_URL}/user/${userData.Username}/change-password`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify(passwordData),
         }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
+
+      await response.json(); // Consume the response body
+      return { success: true, message: "Password changed successfully" };
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 export const removeFavoriteMovie = createAsyncThunk(
-    "user/removeFavoriteMovie",
-    async ({ user, movieId }, { rejectWithValue }) => {
-        try {
-            const encodedMovieId = encodeURIComponent(movieId);
-            const response = await fetch(`${API_URL}/user/${user._id}/${encodedMovieId}`, {
-                method: "DELETE",
-                headers: {
-                    Authorization: `Bearer ${storedToken}`,
-                    "Content-Type": "application/json",
-                },
-            });
-
-            if (response.ok) {
-                const updatedUser = {
-                    ...user,
-                    FavoriteMovies: user.FavoriteMovies.filter((movie) => movie !== movieId),
-                };
-                return updatedUser;
-            } else {
-                const updatedUser = storedUser;
-                return updatedUser;
-            }
-        } catch (error) {
-            return rejectWithValue(error.message);
+  "user/removeFavoriteMovie",
+  async ({ user, movieId }, { rejectWithValue }) => {
+    try {
+      const encodedMovieId = encodeURIComponent(movieId);
+      const response = await fetch(
+        `${API_URL}/user/${user._id}/${encodedMovieId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+            "Content-Type": "application/json",
+          },
         }
+      );
+
+      if (response.ok) {
+        const updatedUser = {
+          ...user,
+          FavoriteMovies: user.FavoriteMovies.filter(
+            (movie) => movie !== movieId
+          ),
+        };
+        return updatedUser;
+      } else {
+        const updatedUser = storedUser;
+        return updatedUser;
+      }
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
