e0bba5a64d3974a3b3ef8155d3b6b6b358f8a76f
danielpinoy <almirante.danieljohn@gmail.com>
2025-02-05 00:15:21 +0200

    refactor

--- src/components/auth/signup-view/signup-view.jsx
+++ src/components/auth/signup-view/signup-view.jsx
@@ -6,73 +6,78 @@ import { useNavigate } from "react-router-dom";
 import { signupUser } from "../../../asyncThunk/userAsyncThunks";
 
 const SignupView = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [birthday, setBirthday] = useState("");
-    const dispatch = useDispatch();
-    const signUpSubmit = (event) => {
-        event.preventDefault();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const dispatch = useDispatch();
+  const signUpSubmit = (event) => {
+    event.preventDefault();
 
-        dispatch(
-            signupUser({ Username: username, Password: password, Email: email, Birthday: birthday })
-        );
-        setUsername("");
-        setPassword("");
-        setEmail("");
-        setBirthday("");
-        navigate("/login");
-    };
-    return (
-        <Form onSubmit={signUpSubmit}>
-            <Form.Group className="mb-3">
-                <Form.Label>Username:</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="ExampleName123"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    minLength="3"
-                />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-                <Form.Label>Password:</Form.Label>
-                <Form.Control
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-                <Form.Label>Email:</Form.Label>
-
-                <Form.Control
-                    type="email"
-                    placeholder="ExampleName123@example.com"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-                <Form.Label>Birthday:</Form.Label>
-                <Form.Control
-                    type="date"
-                    value={birthday}
-                    onChange={(e) => setBirthday(e.target.value)}
-                    required
-                />
-            </Form.Group>
-            <Button type="submit">Submit</Button>
-        </Form>
+    dispatch(
+      signupUser({
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday,
+      })
     );
+    setUsername("");
+    setPassword("");
+    setEmail("");
+    setBirthday("");
+    navigate("/login");
+  };
+  return (
+    <Form onSubmit={signUpSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="ExampleName123"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Email:</Form.Label>
+
+        <Form.Control
+          type="email"
+          placeholder="ExampleName123@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Birthday:</Form.Label>
+        <Form.Control
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+          required
+        />
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
 };
 
 export default SignupView;
--- src/slice/userSlice.js
+++ src/slice/userSlice.js
@@ -1,123 +1,120 @@
 import {
-    signupUser,
-    loginUser,
-    editUser,
-    deleteUser,
-    addFavoriteMovieToUser,
-    removeFavoriteMovie,
-    changePassword,
+  signupUser,
+  loginUser,
+  editUser,
+  deleteUser,
+  addFavoriteMovieToUser,
+  removeFavoriteMovie,
+  changePassword,
 } from "../asyncThunk/userAsyncThunks";
 import { createSlice } from "@reduxjs/toolkit";
 const storedUser = JSON.parse(localStorage.getItem("user"));
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: { user: storedUser || null, loading: false, error: null },
-    reducers: {
-        logout: (state) => {
-            localStorage.clear();
-            state.user = null;
-        },
+  name: "user",
+  initialState: { user: storedUser || null, loading: false, error: null },
+  reducers: {
+    logout: (state) => {
+      localStorage.clear();
+      state.user = null;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(signupUser.fulfilled, (state, action) => {
-                state.user = action.payload;
-                alert("Signup successful");
-                console.log(action.payload);
-            })
-            .addCase(signupUser.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload;
-            })
-            .addCase(loginUser.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(loginUser.fulfilled, (state, action) => {
-                state.loading = false;
-                console.log(action.payload);
-                if (action.payload && action.payload.user !== false) {
-                    state.user = action.payload.user;
-                    localStorage.setItem(
-                        "user",
-                        JSON.stringify(action.payload.user)
-                    );
-                    localStorage.setItem("token", action.payload.token);
-                    window.location.reload();
-                }
-                // Wrong username and password
-                else if (action.payload && action.payload.user === false) {
-                    state.error = "Incorrect Username or Password";
-                }
-            })
-            .addCase(loginUser.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload;
-            })
-            .addCase(editUser.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(editUser.fulfilled, (state, action) => {
-                state.loading = false;
-                state.user = action.payload;
-                localStorage.setItem("user", JSON.stringify(action.payload));
-            })
-            .addCase(editUser.rejected, (state, action) => {
-                state.loading = false;
-                state.error = "Network Problem. Please try again later.";
-            })
-            .addCase(deleteUser.fulfilled, (state) => {
-                state.user = null;
-            })
-            .addCase(deleteUser.rejected, (state, action) => {
-                state.error = "Network Problem. Please try again later.";
-            })
-            .addCase(addFavoriteMovieToUser.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(addFavoriteMovieToUser.fulfilled, (state, action) => {
-                state.loading = false;
-                state.user = action.payload;
-                localStorage.setItem("user", JSON.stringify(action.payload));
-            })
-            .addCase(addFavoriteMovieToUser.rejected, (state, action) => {
-                state.loading = false;
-                state.error = "Network Problem. Please try again later.";
-            })
-            .addCase(removeFavoriteMovie.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(removeFavoriteMovie.fulfilled, (state, action) => {
-                state.loading = false;
-                state.user = action.payload;
-                console.log(action.payload);
-                localStorage.setItem("user", JSON.stringify(state.user));
-            })
-            .addCase(removeFavoriteMovie.rejected, (state, action) => {
-                state.loading = false;
-                state.error = "Network Problem. Please try again later.";
-            })
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signupUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        alert("Signup successful");
+        console.log(action.payload);
+      })
+      .addCase(signupUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        console.log(action.payload);
+        if (action.payload && action.payload.user !== false) {
+          state.user = action.payload.user;
+          localStorage.setItem("user", JSON.stringify(action.payload.user));
+          localStorage.setItem("token", action.payload.token);
+          window.location.reload();
+        }
+        // Wrong username and password
+        else if (action.payload && action.payload.user === false) {
+          state.error = "Incorrect Username or Password";
+        }
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(editUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(editUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        localStorage.setItem("user", JSON.stringify(action.payload));
+      })
+      .addCase(editUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = "Network Problem. Please try again later.";
+      })
+      .addCase(deleteUser.fulfilled, (state) => {
+        state.user = null;
+      })
+      .addCase(deleteUser.rejected, (state, action) => {
+        state.error = "Network Problem. Please try again later.";
+      })
+      .addCase(addFavoriteMovieToUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addFavoriteMovieToUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        localStorage.setItem("user", JSON.stringify(action.payload));
+      })
+      .addCase(addFavoriteMovieToUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = "Network Problem. Please try again later.";
+      })
+      .addCase(removeFavoriteMovie.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(removeFavoriteMovie.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        console.log(action.payload);
+        localStorage.setItem("user", JSON.stringify(state.user));
+      })
+      .addCase(removeFavoriteMovie.rejected, (state, action) => {
+        state.loading = false;
+        state.error = "Network Problem. Please try again later.";
+      })
 
-            // Add cases for changePassword async thunk
-            .addCase(changePassword.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(changePassword.fulfilled, (state, action) => {
-                state.loading = false;
-                if (action.payload.success) {
-                    alert(action.payload.message);
-                }
-            })
-            .addCase(changePassword.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload;
-            });
-    },
+      // Add cases for changePassword async thunk
+      .addCase(changePassword.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(changePassword.fulfilled, (state, action) => {
+        state.loading = false;
+        if (action.payload.success) {
+          alert(action.payload.message);
+        }
+      })
+      .addCase(changePassword.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const { logout } = userSlice.actions;
