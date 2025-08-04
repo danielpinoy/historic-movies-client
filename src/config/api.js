90c408439bdc90eaf71380ab001d8a7fd39f645f
danielpinoy <almirante.danieljohn@gmail.com>
2025-08-04 23:10:10 +0300

    change api

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 const API_CONFIG = {
-  LOCAL_API: "http://localhost:8080/",
-  PRIMARY_API: "http://localhost:8080",
-  FALLBACK_API: "http://localhost:8080",
+  // LOCAL_API: "http://localhost:8080/",
+  PRIMARY_API: "http://94.130.107.9",
+  FALLBACK_API:
+    "https://xo4xjqevs42mbp46utxi3dua3y0lwywt.lambda-url.eu-north-1.on.aws",
   TIMEOUT: 5000,
 };
 
